3051f7706c33253fa4d5a56ba53dcb1f73af538f
ice <ice@lab03m15.nrc.ice.ufjf.br>
2018-04-13 22:28:14 -0300

    Adicionando a chamada ao listar-usuario.jsp

--- src/java/AlbumServlet.java
+++ src/java/AlbumServlet.java
@@ -1,20 +1,29 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "AlbumServlet", urlPatterns = {"/album.html"})
+@WebServlet(name = "AlbumServlet", urlPatterns = {"/usuario.html"})
 public class AlbumServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if ("/album.html".equals(req.getServletPath()))
         {
-            
+            listarUsuarios(req, resp);
         }
+    }
+
+    private void listarUsuarios(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Usuario> usuarios = ListaDeUsuarios.getInstance();
+        req.setAttribute("usuarios", usuarios);
+        RequestDispatcher despachante = req.getRequestDispatcher("/WEB-INF/listar-usuario.jsp");
+        despachante.forward(req, resp);
     }
 
     
