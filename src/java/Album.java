4286d51526d3181da2909a2393d2d71ee88f8be8
ice <ice@lab03m16.nrc.ice.ufjf.br>
2018-04-13 21:34:14 -0300

    Criando a classe Figurinha

--- src/java/Album.java
+++ src/java/Album.java
@@ -1,13 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ice
- */
+import java.util.List;
+
 public class Album {
+    String nome;
+    private List<Figurinha> figurinhas;
+    private List<Figurinha> repetidas;
+    private List<Figurinha> possuidas;
+
+    public Album(List<Figurinha> figurinhas, List<Figurinha> repetidas, List<Figurinha> possuidas) {
+        this.figurinhas = figurinhas;
+        this.repetidas = repetidas;
+        this.possuidas = possuidas;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Figurinha> getFigurinhas() {
+        return figurinhas;
+    }
+
+    public void setFigurinhas(List<Figurinha> figurinhas) {
+        this.figurinhas = figurinhas;
+    }
+
+    public List<Figurinha> getRepetidas() {
+        return repetidas;
+    }
+
+    public void setRepetidas(List<Figurinha> repetidas) {
+        this.repetidas = repetidas;
+    }
+
+    public List<Figurinha> getPossuidas() {
+        return possuidas;
+    }
+
+    public void setPossuidas(List<Figurinha> possuidas) {
+        this.possuidas = possuidas;
+    }
+    
+    
     
 }
---
+++ src/java/Figurinha.java
@@ -0,0 +1,27 @@
+public class Figurinha {
+    Integer numero;
+    String nome;
+
+    public Figurinha(Integer numero, String nome) {
+        this.numero = numero;
+        this.nome = nome;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
