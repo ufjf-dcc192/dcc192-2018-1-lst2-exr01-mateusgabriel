9e90ab18bc31b5d8db175b560a30cb24f94f4ec8
ice <ice@lab03m16.nrc.ice.ufjf.br>
2018-04-13 21:44:19 -0300

    Criando a classe usuário e corrigindo a classe album

--- src/java/Album.java
+++ src/java/Album.java
@@ -1,16 +1,37 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Album {
-    String nome;
-    private List<Figurinha> figurinhas;
-    private List<Figurinha> repetidas;
-    private List<Figurinha> possuidas;
+    String nome = "Copa do Mundo 2018";
+    private static List<Figurinha> figurinhas;
+    private static List<Figurinha> repetidas;
+    private static List<Figurinha> possuidas;
+    
+    public static List<Figurinha> getInstance() {
+        if (figurinhas == null) {
+            figurinhas = new ArrayList<>();
+            figurinhas.add(new Figurinha(1, "Primeira Tarefa"));
+            figurinhas.add(new Figurinha(2, "Segunda Tarefa"));
+            figurinhas.add(new Figurinha(3, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(4, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(5, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(6, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(7, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(8, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(9, "Tarefa Tarefa"));
+            figurinhas.add(new Figurinha(10, "Tarefa Tarefa"));
+        }
+        return figurinhas;
+    }
 
     public Album(List<Figurinha> figurinhas, List<Figurinha> repetidas, List<Figurinha> possuidas) {
         this.figurinhas = figurinhas;
         this.repetidas = repetidas;
         this.possuidas = possuidas;
+    }
+
+    public Album() {
     }
     
     
---
+++ src/java/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    Album a = new Album();
+
+    public Album getA() {
+        return a;
+    }
+
+    public void setA(Album a) {
+        this.a = a;
+    }
+}
